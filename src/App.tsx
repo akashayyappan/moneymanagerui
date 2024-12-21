a93b7c5fadc0ea3e504b7daeb123572a706236c5
akashayyappan <akashayyappan@outlook.com>
2024-12-21 06:02:14 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Header from './component/header';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header></Header>
+    </React.Fragment>
+  );
+}
+
+export default App;
